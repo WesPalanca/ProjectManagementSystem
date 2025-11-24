f63160c7af99e39550407c770f060e6b97d38f89
WesPalanca <wespalanca@gmail.com>
2025-11-24 12:57:00 -0500

    updated project task and user models

---
+++ ProjectManagementSystem/src/Models/ProjectTask.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectManagementSystem.Models;
+using ProjectManagementSystem.Enums;
+
+public abstract class ProjectTask
+{
+    public int TaskId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int AssignedBy { get; set; } // UserId
+    public int AssignedTo { get; set; } // UserId
+    public ProjectTaskStatus Status { get; set; }
+    public DateTime Deadline { get; set; }
+    public string TaskType { get; set; }
+
+    public ProjectTask(string title, string description, int assignedBy, int assignedTo, DateTime deadline,
+        string taskType)
+    {
+        Title = title;
+        Description = description;
+        AssignedBy = assignedBy;
+        AssignedTo = assignedTo;
+        Status = ProjectTaskStatus.Unassigned;
+        Deadline = deadline;
+        TaskType = taskType;
+    }
+  
+
+}
